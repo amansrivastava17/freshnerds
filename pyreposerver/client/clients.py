40f7b6b0b5c95bf374bf0def93ea4b978f362a3c
amansrivastava17 <aman_srivastava17@rediff.com>
2014-11-18 14:19:27 +0530

    newproject

---
+++ pyreposerver/client/clients.py
@@ -0,0 +1,107 @@
+import socket
+import os
+import json 
+
+setupfile=open('setup.json','rb')
+read=setupfile.read()
+read=json.loads(read)
+
+dest_folder=read['directory_upload']
+HOST=read['host']
+PORT_NO=read['port']
+
+def create_dir(dir_struct):
+	for item in dir_struct:
+		l=item.split('/')
+		f="/"
+		for item2 in l:
+			if not os.path.exists(dest_folder+f+item2):
+				os.mkdir(dest_folder+f+item2)
+				f=f+item2+"/"
+			else:
+				f=f+item2+"/"
+
+
+def receiving(s):
+	s.send("allowed")
+	data2=s.recv(1024)
+	filename=data2[12:]
+	s.send("send size")
+	k=s.recv(1024)
+	filesize=long(k[4:])
+	print filesize
+	s.send("send file")
+		#f.write(data)
+	f=open(dest_folder+"/"+filename,'wb')
+	print "downloading file "+data2.split('/')[-1]
+	#print "receivingggg"
+	data=s.recv(1024)
+	if data=="null":
+		f.close()
+	else:
+		totalrecv=len(data)
+		f.write(data)
+		while totalrecv<filesize:
+			#print "55"
+			data=s.recv(1024)
+			totalrecv+=len(data)
+			f.write(data)
+			print '{0:2f}'.format((totalrecv/float(filesize))*100)+"% Done"
+		f.close()
+	s.send("send next")
+	nexti=s.recv(1024)
+	#print nexti
+	if nexti== "next file":
+		#print "2"
+		receiving(s)
+	else:
+		return
+
+
+def Main():
+	host=HOST
+	port=PORT_NO
+
+	s=socket.socket()
+	s.connect((host,port))
+
+	filename=raw_input("Enter File-->");
+	if filename !='q':
+		s.send(filename)
+		data=s.recv(1024)
+		if data[:6]=="filess":
+			filesize=long(data[6:])
+			message=raw_input('File exists,'+str(filesize)+'Bytes ,download(Y/N)?==>')
+			if (message=='Y')|(message=='y'):
+				s.send('OK')
+				f=open(dest_folder+'/'+filename,'wb')
+				data=s.recv(1024)
+				totalrecv=len(data)
+				f.write(data)
+				while totalrecv<filesize:
+					data=s.recv(1024)
+					totalrecv+=len(data)
+					f.write(data)
+					print "{0:2f}".format((totalrecv/float(filesize))*100)+\
+							"% Done"
+				f.close()
+			print "Download complete"
+
+		elif data[:6]=="folder":
+			#filesize=long(data[6:])
+			message=raw_input('Folder exists,download(Y/N)?==>')
+			if (message=='Y')| (message=='y'):
+				s.send('OK')
+				file_structure=s.recv(1024)
+				create_dir(file_structure.split('^'))
+				
+				#f=open('//home/aman/Desktop/new_'+filename,'wb')
+				receiving(s)
+			print "Download complete"
+				
+		else:
+			print "File does not exists!"
+	s.close()
+
+if __name__=='__main__':
+	Main()
---
+++ pyreposerver/server/server.py
@@ -0,0 +1,114 @@
+import os
+import threading
+import socket
+import json 
+
+setupfile=open('setup.json','rb')
+read=setupfile.read()
+read=json.loads(read)
+
+current_dir=read['directory_share']
+PORT_NO=read['port']
+
+lenth=len(current_dir)
+def dir_struct(roots):
+	dir_path_list=[]
+	for root,directory,files in os.walk(roots):
+		for dire in directory:
+			dirpath=os.path.join(root,dire)
+			dir_path_list.append(dirpath[lenth:])
+
+	return dir_path_list
+		
+def file_list_all(roots):
+	file_path_list=[]
+	for root,directory,files in os.walk(roots):
+		for filename in files:
+			filepath=os.path.join(root,filename)
+			file_path_list.append(filepath[lenth:])	
+	return file_path_list
+		
+def recievefile(name,socket):
+	filename=socket.recv(1024)
+	
+	if os.path.isfile(current_dir+filename):
+		socket.send("filess"+str(os.path.getsize(current_dir+filename)))
+		userresponse=socket.recv(1024)
+		if userresponse[:2]=='OK':
+			#print "1"
+			with open(current_dir+filename,'rb') as f:
+				bytestosend=f.read(1024)
+				socket.send(bytestosend)
+				while bytestosend !="":
+					bytestosend=f.read(1024)
+					socket.send(bytestosend)
+			f.close()
+	elif os.path.isdir(current_dir+filename):
+		socket.send("folder"+str(os.path.getsize(current_dir+filename)))
+		#current_dir=os.getcwd()+"/"
+		userresponse=socket.recv(1024)
+		if userresponse[:2]=='OK':
+			filestruct=dir_struct(current_dir+filename)
+			print filestruct
+			filestruct="^".join(filestruct)
+			socket.send(filestruct)
+			file_list=file_list_all(current_dir+filename)
+			print file_list
+			for files in file_list:
+				permission=socket.recv(1024)
+				if permission=="allowed":
+					socket.send("sending file"+files)
+					userresponse2=socket.recv(1024)
+					if userresponse2=="send size":
+						#print current_dir+files
+						filesize=os.path.getsize(current_dir+files)
+						#print filesize
+						socket.send("size"+str(filesize))
+						send=socket.recv(1024)
+						if send == "send file":
+							with open(current_dir+files,'rb') as f:
+								#print "1"
+								bytestosend=f.read(1024)
+								#print "15555"
+								#socket.send(bytestosend)
+								if bytestosend!="":
+									while bytestosend !="":
+									#print "2"
+										socket.send(bytestosend)
+										bytestosend=f.read(1024)
+									#print "3"
+								else:
+									socket.send("null")
+
+								f.close()
+
+
+								check=socket.recv(1024)
+								if check=="send next":
+									if files !=file_list[-1]:
+										#print files
+										socket.send("next file")
+									else:
+										socket.send("over")
+
+	else:
+		socket.send("Err")
+
+def Main():
+	host='127.0.0.1'
+	port=PORT_NO
+	s=socket.socket()
+	s.bind((host,port))
+	s.listen(5)
+
+	print "server started"
+
+	while True:
+		c,addr=s.accept()
+		print "client connected <"+str(addr)+">"
+		t=threading.Thread(target=recievefile,args=('retrieve',c))
+		t.start()
+	s.close()
+
+if __name__=='__main__':
+	Main()
